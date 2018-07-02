e90c1ee418fa9cc30404f26bc1bd66a28c257843
Diego Silvera <diegosilvera@MacBook-Air-de-Diego.local>
2018-07-02 13:02:10 -0300

    Se agregan funciones para evaluar comportamiento de ts vs js

--- typescript/declaration/jsdeclaration.js
+++ typescript/declaration/jsdeclaration.js
@@ -4,3 +4,14 @@
 var dName = "diego";
 var dAge = 22;
 var dSaved = true;
+
+/**
+ * Javascript functions
+ */
+function getJsPassword(clearTextPassword) {
+    if (clearTextPassword) {
+        return "password";
+    }
+
+    return "********";
+}
--- typescript/declaration/tsdeclaration.ts
+++ typescript/declaration/tsdeclaration.ts
@@ -4,3 +4,14 @@
 let tName: string = "diego";
 let tAge: number = 22;
 let tSaved: boolean = true;
+
+/**
+ * Typescript functions
+ */
+function getTsPassword(clearTextPassword: boolean): string {
+    if (clearTextPassword) {
+        return "password";
+    }
+
+    return "********";
+}
