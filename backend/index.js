ec28b30bf204c1f480f5e787dc17311cfd08fe1b
Diego Silvera de Souza <ctk.591@gmail.com>
2018-07-09 00:33:45 -0300

    Se crea backend en node js

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+﻿var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello world!");
+});
+
+app.listen(3000, function() {
+    console.log("App is running in port 3000");
+});
